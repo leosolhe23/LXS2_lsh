9e48c0b4a3b2d2177c5b28b00a3530b743af29a2
Leonardo Solano H <lesolh23@gmail.com>
2016-11-05 11:48:50 -0600

    Agrego al sistema de reportes las salidas de ls interfaces

--- src/reporte.sh
+++ src/reporte.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 REPORTE=reporte-`date +%Y%m%d`.log
 
-
 echo  "Reporte generado por  $USER:"  >  $REPORTE
 
 echo " " >>  $REPORTE
+echo " " >>  $REPORTE
+
+echo "########################################" >> $REPORTE
+echo "#Interfaces disponibles en el servidor #"  >> $REPORTE
+echo "########################################" >> $REPORTE
+
+echo " " >>  $REPORTE
+
+ifconfig >> $REPORTE
+
+
+echo "########################################" >> $REPORTE
+echo "# Puertos habilidatos en el servidor   #" >> $REPORTE
+echo "########################################" >> $REPORTE
+
+
+netstat -latun | grep LISTEN >> $REPORTE
