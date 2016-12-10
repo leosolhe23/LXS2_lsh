04af7650f8d268a51ca4efbba6b92a355f74b4d7
Roberto Zuniga <robzuniga@mac.com>
2016-12-10 10:59:55 -0600

    commit bash if

---
+++ src/if-anidado.sh
@@ -0,0 +1,49 @@
+#/bin/bash
+
+## este script requiere tres parametros
+## ./resumen.sh  <param1>  <param2> <param3>
+
+# Definicion de variables
+
+PARAMETROS=3
+VERSION_BASH="4.3.11"
+
+# Evaluar si la distro es ubuntu y si la version de bash es 4.3.11
+
+if [[ `echo  $BASH_VERSION | cut -d "(" -f1` == $VERSION_BASH  ||
+	`lsb_release -i | awk '{print $3}'` == Ubuntu ]] 
+then
+	echo "Version de bash $VERSION_BASH correcta!"
+else
+	echo "Error:
+	Si version de bash no es compatible con el script.
+	Necesita la version $VERSION_BASH y esta instalada
+ 	la version $BASH_VERSION"
+	exit 1
+fi
+
+# Para diferencias uso !=
+if [[ $# != $PARAMETROS ]] 
+then
+	echo "Error:
+	La forma de utilizar el script es la siguiente
+        $0 <par1> <par2> <par3> 
+	"
+	exit 1
+else
+	echo " El numero de parametros es correcto, y fue $#"
+ 	if  [[ $2 == casa ]] 
+	then
+		echo "El parametro 2 es casa!"
+	else
+		echo "El parametro 2 NO es casa!"
+	fi
+fi
+
+echo "Los parametros fueron "
+echo $@
+
+
+
+exit 0
+
